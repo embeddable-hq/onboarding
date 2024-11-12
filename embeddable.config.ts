a85fe1fe17875f982c0bfb5249fa04fb52aae298
Tom Gardiner <tom@trevor.io>
2024-11-12 12:37:48 +0100

    add US and EU deployment sections to embeddable.config.ts

--- embeddable.config.ts
+++ embeddable.config.ts
@@ -5,5 +5,20 @@ export default defineConfig({
   plugins: [react],
   modelsSrc: './src/embeddable.com/models',
   componentsSrc: './src/embeddable.com/components',
-  errorFallbackComponent: './src/embeddable.com/components/util/ErrorHandler.jsx'
+  errorFallbackComponent: './src/embeddable.com/components/util/ErrorHandler.jsx',
+
+  // 
+  // Uncomment for US deployments
+  //
+  // previewBaseUrl: 'https://app.us.embeddable.com',
+  // pushBaseUrl: 'https://api.us.embeddable.com',
+  
+  //
+  // Uncomment for EU deployments
+  //
+  // previewBaseUrl: 'https://app.eu.embeddable.com',
+  // pushBaseUrl: 'https://api.eu.embeddable.com',
+  // audienceUrl: 'https://auth.eu.embeddable.com',
+  // authDomain: 'auth.eu.embeddable.com',
+  // authClientId: '6OGPwIQsVmtrBKhNrwAaXhz4ePb0kBGV',
 });
